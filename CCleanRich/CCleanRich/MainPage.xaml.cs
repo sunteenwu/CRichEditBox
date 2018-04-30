1bd3e8de9c218a58ecc219d43540c9e09a3f1a66
sunteenwu <wu_15xun@163.com>
2018-04-30 10:48:07 +0800

    Testing for LoadFromStream method.

---
+++ CCleanRich/CCleanRich/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CCleanRich
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Loadfile_Click(object sender, RoutedEventArgs e)
+        {
+            //StorageFolder folder = ApplicationData.Current.LocalFolder;
+            //StorageFile file = await folder.GetFileAsync("test1.rtf");
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/test1.rtf"));
+            //StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/test2.rtf"));
+            IRandomAccessStream randAccStream = await file.OpenAsync(FileAccessMode.Read);
+            Richbox.Document.LoadFromStream(TextSetOptions.FormatRtf, randAccStream);
+        }
+    }
+}
